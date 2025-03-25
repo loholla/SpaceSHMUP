885c455d05ab214ac81a7674ad06c0382b17092d
Shadow-Diamond <147402627+loholla@users.noreply.github.com>
2025-03-25 15:22:17 -0500

    Hero Movement

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    static public Hero  S { get; private set;}
+    [Header("Inscribed")]
+
+    public float    speed = 30;
+    public float    rollMult = -45;
+    public float    pitchMult = 30;
+
+    [Header("Dynamic")] [Range(0,4)]
+    public float    shieldLevel = 1;
+
+    void Awake() {
+        if (S == null) {
+            S = this;
+        } else {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += hAxis * speed * Time.deltaTime;
+        pos.y += vAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(vAxis*pitchMult,hAxis*rollMult,0);
     }
 }
