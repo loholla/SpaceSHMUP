87a3550f3734d0ef50f344fa38dfdd31d0a3b806
Shadow-Diamond <147402627+loholla@users.noreply.github.com>
2025-03-25 16:38:45 -0500

    Enemy_0 Done

--- Assets/__Scripts/BoundsCheck.cs
+++ Assets/__Scripts/BoundsCheck.cs
@@ -1,18 +1,28 @@
 using UnityEngine;
 
 /// <summary>
-/// Keeps a GameObject on screen.
+/// Keeps a GameObject on screen and can force it to stay on screen.
 /// Note that this ONLY works for an orthographic Main Camera
 /// </summary>
 public class BoundsCheck : MonoBehaviour
 {
+    [System.Flags]
+    public enum eScreenLocs{
+        onScreen = 0,
+        offRight = 1,
+        offLeft = 2,
+        offUp = 4,
+        offDown = 8
+    }
     public enum eType {center, inset, outset};
 
     [Header("Inscribed")]
     public eType boundsType = eType.center;
     public float radius = 1f;
+    public bool  keepOnScreen = true;
 
     [Header("Dynamic")]
+    public eScreenLocs screenLocs = eScreenLocs.onScreen;
     public float camWidth;
     public float camHeight;
 
@@ -28,20 +38,37 @@ public class BoundsCheck : MonoBehaviour
         if (boundsType == eType.inset) checkRadius = -radius;
         if (boundsType == eType.outset) checkRadius = radius;
         Vector3 pos = transform.position;
+        screenLocs = eScreenLocs.onScreen;
 
         if (pos.x > camWidth + checkRadius){
             pos.x = camWidth + checkRadius;
+            screenLocs |= eScreenLocs.offRight;
         }
         if (pos.x < -camWidth - checkRadius){
             pos.x = -camWidth - checkRadius;
+            screenLocs |= eScreenLocs.offLeft;
         }
         if (pos.y > camHeight + checkRadius){
             pos.y = camHeight + checkRadius;
+            screenLocs |= eScreenLocs.offUp;
         }
         if (pos.y < -camHeight - checkRadius){
             pos.y = -camHeight - checkRadius;
+            screenLocs |= eScreenLocs.offDown;
         }
 
-        transform.position = pos;
+        if (keepOnScreen && !isOnScreen){
+            transform.position = pos;
+            screenLocs |= eScreenLocs.onScreen;
+        }
+    }
+
+    public bool isOnScreen {
+        get { return (screenLocs == eScreenLocs.onScreen);}
+    }
+
+    public bool LocIs (eScreenLocs checkLoc){
+        if (checkLoc == eScreenLocs.onScreen) return isOnScreen;
+        return ((screenLocs & checkLoc) == checkLoc);
     }
 }
---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Enemy : MonoBehaviour
+{
+   [Header("Inscribed")]
+   public float speed    = 10f;
+   public float fireRate = 0.3f;
+   public float health   = 10;
+   public int   score    = 100;
+
+   private BoundsCheck bndCheck;
+
+    void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    public Vector3 pos{
+        get{
+            return this.transform.position;
+        }
+        set{
+            this.transform.position = value;
+        }
+   }
+
+    void Update()
+    {
+        Move();
+        if (bndCheck.LocIs(BoundsCheck.eScreenLocs.offDown)){
+            Destroy(gameObject);
+        }
+        // if (!bndCheck.isOnScreen){
+        //     if (pos.y < bndCheck.camHeight - bndCheck.radius){
+        //         Destroy(gameObject);
+        //     }
+        // }
+    }
+
+    public virtual void Move(){
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+}
