99973fabb9adeb2650748c705adc6e5a5b1abd73
Shadow-Diamond <147402627+loholla@users.noreply.github.com>
2025-03-25 19:46:14 -0500

    Enemy Spawning Done

---
+++ Assets/__Scripts/Main.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    static private Main S;
+
+    [Header("Inscribed")]
+    public GameObject[] prefabEnemies;
+    public float enemySpawnPerSecond = 0.5f;
+    public float enemyInsetDefault = 1.5f;
+
+    private BoundsCheck bndCheck;
+
+    void Awake()
+    {
+        S = this;
+        bndCheck = GetComponent<BoundsCheck>();
+
+        Invoke(nameof(SpawnEnemy), 1f/enemySpawnPerSecond);
+    }
+
+    public void SpawnEnemy(){
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+
+        float enemyInset = enemyInsetDefault;
+        if (go.GetComponent<BoundsCheck>() != null){
+            enemyInset = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
+        }
+
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camWidth + enemyInset;
+        float xMax = bndCheck.camWidth - enemyInset;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyInset;
+        go.transform.position = pos;
+
+        Invoke(nameof(SpawnEnemy), 1f/enemySpawnPerSecond);
+    }
+}
