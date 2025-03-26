a71f6d1743ea665e945a9ed006789e5a98a48fa8
Shadow-Diamond <147402627+loholla@users.noreply.github.com>
2025-03-25 19:58:01 -0500

    Hero Death Added

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -10,7 +10,10 @@ public class Hero : MonoBehaviour
     public float    pitchMult = 30;
 
     [Header("Dynamic")] [Range(0,4)]
-    public float    shieldLevel = 1;
+    private float   _shieldLevel = 1;
+
+    [Tooltip("This field holds a reference to the last triggering GameObject")]
+    private GameObject lastTriggerGo = null;
 
     void Awake() {
         if (S == null) {
@@ -30,5 +33,31 @@ public class Hero : MonoBehaviour
         transform.position = pos;
 
         transform.rotation = Quaternion.Euler(vAxis*pitchMult,hAxis*rollMult,0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        if(go == lastTriggerGo) return;
+        lastTriggerGo = go;
+
+        Enemy enemy = go.GetComponent<Enemy>();
+        if (enemy != null){
+            shieldLevel--;
+            Destroy(go);
+        } else {
+            Debug.LogWarning("Shield trigger hit by non-Enemy: "+go.name);
+        }
+    }
+
+    public float shieldLevel{
+        get {return (_shieldLevel);}
+        private set{
+            _shieldLevel = Mathf.Min(value, 4);
+            if (value < 0) {
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
